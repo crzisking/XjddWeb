f791978fafee63dd7e8274fe7be0e10f8adafab6
Jacky.Chen <135211647+crzisking@users.noreply.github.com>
2024-12-13 20:50:27 +0800

    refactor(master): 完成首页菜单状态管理

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-import {GetMenuList} from "./api/init/init.ts";
-import {MenuItem,useMenuStore} from "./stores/MenuStore.ts";
-import {onMounted} from "vue";
-const MenuStore =useMenuStore()
-onMounted(
-  ()=>{
-    GetMenu()
-  }
-)
+import { GetMenuList } from "./api/init/init.ts";
+import { MenuItem, useMenuStore } from "./stores/MenuStore.ts";
+import { onMounted } from "vue";
+
+const MenuStore = useMenuStore();
+
 const GetMenu = () => {
+  //判断是否已经获取过菜单
+  if (MenuStore.menuList.length > 0) {
+    return;
+  }
   GetMenuList().then((res) => {
     if (res.code === 200) {
-      const menuItems: MenuItem[] = JSON.parse(res.data); // 显式指定解析后的结果类型
-      menuItems.forEach((item) => {
-        MenuStore.addMenu(item); // 假设 addMenu 接受 MenuItem 类型的参数
-      });
-      console.log(menuItems)
+      const menuItems : Array<MenuItem> = res.data; // 直接解析为 MenuItem[]
+      MenuStore.setMenuList(menuItems);
     }
   });
 };
+
+onMounted(() => {
+  GetMenu();
+});
 </script>
 
 <template>
--- src/api/init/init.ts
+++ src/api/init/init.ts
@@ -1,14 +1,15 @@
 import request from "../request.ts";
+import {MenuItem} from "../../stores/MenuStore.ts";
 interface GetMenuResponse {
     code: number;
     msg: string;
-    data: string; // 假设返回的数据是一个 JSON 字符串，包含菜单项
+    data: Array<MenuItem>; // 假设返回的数据是一个 JSON 字符串，包含菜单项
 }
 
 // 定义 GetMenuList 函数，返回类型为 GetMenuResponse
 export const GetMenuList = async (): Promise<GetMenuResponse> => {
     return await request({
-        url: '/api/menu',
+        url: '/api/init/GetMenuList',
         method: 'get'
     })
 };
